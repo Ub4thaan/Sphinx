a507fb434fe04e2e28c65cf3248919d4e090009d
Daniel Martin <daniel.mrt.96@gmail.com>
2021-05-11 20:52:17 +0200

    Added Base TextBox viewmodel

---
+++ Sphinx/ViewModels/TextBoxViewModels.cs
@@ -0,0 +1,33 @@
+﻿using Sphinx.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sphinx.ViewModels
+{
+    public class TextBoxViewModels : BaseViewModel
+    {
+        public virtual string Text 
+        {
+            get
+            {
+                return _Text;
+            }
+            set
+            {
+                if (_Text != value)
+                {
+                    _Text = value;
+                    RaisePropertyChanged(nameof(Text));
+                }
+            }
+        }
+
+        private string _Text;
+    }
+
+    
+}
