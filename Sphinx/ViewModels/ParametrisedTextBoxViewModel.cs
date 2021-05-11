0a603627aab565382d58fbdd125c6fc4ca8af05d
Daniel Martin <daniel.mrt.96@gmail.com>
2021-05-11 20:52:31 +0200

    Added parametrised TextBox viewmodel

---
+++ Sphinx/ViewModels/ParametrisedTextBoxViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Sphinx.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sphinx.ViewModels
+{
+    public class TextBoxViewModels<T> : BaseViewModel
+    {
+        public T Value
+        {
+            get
+            {
+                return _Value;
+            }
+            set
+            {
+                _Value = value;
+                RaisePropertyChanged(nameof(Value));
+            }
+        }
+
+        private T _Value;
+    }
+}
