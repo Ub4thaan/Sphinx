93d3cccade81b1bdc9e4be4d06b626a1b8473f5c
Daniel Martin <daniel.mrt.96@gmail.com>
2021-05-12 10:30:01 +0200

    Added basic button view model

---
+++ Sphinx/ViewModels/ButtonViewModels.cs
@@ -0,0 +1,61 @@
+﻿using Sphinx.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sphinx.ViewModels
+{
+    public class ButtonViewModels : BaseViewModel
+    {
+        public string Content
+        {
+            get
+            {
+                return _Content;
+            }
+            set
+            {
+                if (_Content != value)
+                {
+                    _Content = value;
+                    RaisePropertyChanged(nameof(Content));
+                }
+            }
+        }
+        public bool IsEnabled
+        {
+            get
+            {
+                return _IsEnabled;
+            }
+            set
+            {
+                if (_IsEnabled != value)
+                {
+                    _IsEnabled = value;
+                    RaisePropertyChanged(nameof(IsEnabled));
+                }
+            }
+        }
+        public bool IsVisible
+        {
+            get
+            {
+                return _IsVisible;
+            }
+            set
+            {
+                if (_IsVisible != value)
+                {
+                    _IsVisible = value;
+                    RaisePropertyChanged(nameof(IsVisible));
+                }
+            }
+        }
+
+        private string _Content;
+        private bool _IsEnabled;
+        private bool _IsVisible;
+    }
+}
